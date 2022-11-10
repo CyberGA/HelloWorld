57d47d7afa2a13df5571784d64b072c24c079eae
CyberGA <etomu.joshua@gmail.com>
2022-11-10 05:48:40 +0100

    mutable references

--- src/printer.rs
+++ src/printer.rs
@@ -166,11 +166,31 @@ pub fn print_arr(arr: &mut[i32;4]) {
 
   // this will working because we passed by reference
   // and the ownership is transferred back to the originally object
-  let v = vec![10,20,30];
-  print_vector(&v); // passing reference
-  println!("Printing the value from main() v[0]={}",v[0]);
+  // let v = vec![10,20,30];
+  // print_vector(&v); // passing reference
+  // println!("Printing the value from main() v[0]={}",v[0]);
 
-  fn print_vector(x:&Vec<i32>){
-    println!("Inside print_vector function {:?}",x);
-  }
+  // fn print_vector(x:&Vec<i32>){
+  //   println!("Inside print_vector function {:?}",x);
+  // }
+
+  // function can modify a borrowed resource by using a mutable reference to such resource
+  // fn add_one(e: &mut i32) {
+  //   *e+= 1;
+  // }
+  // let mut i = 3;
+  //  add_one(&mut i);
+  //  println!("{}", i);
+
+
+  // mutating a string
+  fn display(param_name:&mut String){
+    println!("param_name value is :{}",param_name);
+    param_name.push_str(" Rocks"); 
+    //Modify the actual string,name
+   }
+   let mut name:String = String::from("TutorialsPoint");
+   display(&mut name); 
+   //pass a mutable reference of name
+   println!("The value of name after modification is:{}",name);
  }
