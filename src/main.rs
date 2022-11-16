43a2ee956a2fc24d759cfa9239af4bbe7682093a
CyberGA <etomu.joshua@gmail.com>
2022-11-16 18:00:46 +0100

    working with generic functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod printer;
 
 fn main() {
-    printer::using_traits()
+    printer::proprinter(10 as u32);
+    printer::proprinter(20 as u16);
+    printer::proprinter(30 as u8);
+    printer::proprinter("Test");
 }
 
 #[test]
