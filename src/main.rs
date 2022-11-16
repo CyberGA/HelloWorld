e31e7dbeb99144875d86c8c509617ff601045d55
CyberGA <etomu.joshua@gmail.com>
2022-11-16 18:12:56 +0100

    read and write using the stdin & stdout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 mod printer;
 
 fn main() {
-    printer::proprinter(10 as u32);
-    printer::proprinter(20 as u16);
-    printer::proprinter(30 as u8);
-    printer::proprinter("Test");
+    printer::read_write();
 }
 
 #[test]
