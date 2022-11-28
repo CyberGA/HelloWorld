7e8d06f342eb571fe971c37e95b8ddc0b2b9d03a
CyberGA <etomu.joshua@gmail.com>
2022-11-28 16:37:35 +0100

    using generics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod printer;
 
 fn main() {
-    printer::using_struct();
+    printer::proprinter(10);
+    printer::proprinter("Gbenga Etomu");
+    printer::proprinter('A');
+    printer::proprinter(true);
+    printer::proprinter(10.2);
 }
 
 #[test]
